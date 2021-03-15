0d53847ee6037b1eb6c776425eae9202b882b3b1
Sean Elia <selia1@umbc.edu>
2021-03-15 05:06:08 -0400

    I forgot to make additional commits while working on the project. This is the final commit before submission of the assignment.

--- README.md
+++ README.md
@@ -1,24 +1,39 @@
 # CMSC447IndividualAssignment
 Author: Sean Elia
 
-This Repository contains a Python virtual environment with all the code needed to create and interact with a database table
+This Repository contains files to add to a python virtual library in order to set up the project
+These need to be downloaded for the project to function:
+- python 3+
+- mySQL (and preferably mySQL Workbench)
 
-The following libraries (and any they are dependant on) were added to the virtual environment using pip:
+The following libraries (and any they are dependant on) must be added to the virtual environment using pip:
 - flask
 - react
-- sqlAlchemy
+- mysql.connector
 - pandas
 
-Files not auto-generated in virtual environment:
+Files that must be added to the virtual environment (assume venv was named *myVenv*):
+(Note: repository contains some unnecesary files, however these are the only files that need to be added)
 
 *myVenv*:
 - app.py
 - create.sql
 - drop.sql
 - populate.sql
-- tmp.csv
+- config.txt
 - *Templates*:
     - edit.html
     - home.html
     - view.html
-    - table.html
+
+How to set up the Database portion:
+- Using mySQL Workbench, host a Database and run the included files *create.sql* then *populate.sql* on the server to initialize the table
+    - If not using mySQL Workbench, then create the server and copy the scripts into a comand line
+- Update the contents of *config.txt* to contain the name of your Database, the username and pasword, and the IP address/port it is connected to
+    - Ensure that each data field listed below is on a seperate line of the file and formated as "< data field >: < value specific to your database >":
+        - dbName        (The name you assined to the Database server as a whole)
+        - dbIP          (The IP address the Database can be accessed at)
+        - dbPort        (Which port the Database must be accessed from)
+        - dbUser        (The username to access your Database)
+        - dbPass        (The password to access your Database)
+    - The order of these data fields is irrelavent, however the format and inclusion of each is very important
--- myVenv/app.py
+++ myVenv/app.py
@@ -6,10 +6,52 @@
 # A python file used to set up flask and react
 #-----------------------------------------------------------------------
 
-from flask import Flask, render_template
+from flask import Flask, render_template, json
+import mysql.connector
 import pandas as pd
 
 app = Flask(__name__)
+
+def getConnection():
+    # settings default, please update the formated file "config.txt" to contain the correct settings
+    settings = {
+        "dbName": "Students",            # Name of the databse (not table) to connect to
+        "dbIP": "localhost",             # IP address for the data base
+        'dbPort': 3306,                  # port number for the connection
+        "dbUser": "user",                # Username for accessing the database
+        "dbPass": "password",            # Password for accessing the database
+    }
+
+    # Gets default font from Config.txt
+    try:
+        # Reads File
+        configFile = open("./config.txt", "r")
+
+        for line in configFile:
+            splitLine = (line.strip()).split(': ')
+            if (splitLine[0] == "dbPort"):
+                settings[splitLine[0]] = int(splitLine[1])
+            else:
+                settings[splitLine[0]] = splitLine[1]
+
+        configFile.close() 
+
+    except Exception as e:
+        print(e)
+
+    return settings
+
+settings = getConnection()
+config = {
+    'user': settings["dbUser"],
+    'password': settings["dbPass"],
+    'host': settings["dbIP"],
+    'port': settings["dbPort"],
+    'database': settings["dbName"],
+    'raise_on_warnings': True                    
+}
+#db_connection = mysql.connector.connect(**config)
+#cur = db_connection.cursor()
 
 @app.route('/')
 def index():
@@ -21,16 +63,71 @@ def home():
 
 @app.route('/view')
 def view():
-    df = pd.read_csv("./tmp.csv")
-    html = df.to_html()
-    text_file = open("./Templates/table.html", "w") 
-    text_file.write(html) 
-    text_file.close() 
-    return render_template("view.html")
+    df = pd.read_sql("SELECT studentId AS id, studentName AS name, studentMarks AS marks FROM studentTable" , con=db_connection)
+    return render_template("view.html", data = df.to_html())
 
-@app.route('/edit')
+@app.route('/getmethod/<jsdata>')
+def get_javascript_data(jsdata):
+    return jsdata
+
+@app.route('/edit', methods = ['POST'])
 def edit():
-    return render_template("edit.html")
+    num = -1
+    studentId = []
+    studentName = []
+    studentMarks = []
+    if request.method == 'POST':
+        stat = request.form['stat']
+        idS = request.form['id']
+        nmS = request.form['nm']
+        mkS = request.form['mk']
+        if stat:
+            if stat == 0:       # Add
+                if idS and nmS and mkS and int(ids) != -1:
+                    cur.execute(f"INSERT INTO studentTable VALUES ({int(idS)}, {nmS}, {int(mkS)});")
+                cur.execute("SELECT * FROM studentTable")
+                for (idS, nameS, marksS) in cur:
+                    studentId.append(idS)
+                    studentName.append(nameS)
+                    studentMarks.append(marksS)
+            elif stat == 1:     # Update
+                if idS and nmS and mkS and int(ids) != -1:
+                    cur.execute(f"UPDATE studentTable SET studentName = {nmS}, studentMarks = {int(mkS)} WHERE studentId = {int(idS)};")
+                cur.execute("SELECT * FROM studentTable")
+                for (idS, nameS, marksS) in cur:
+                    studentId.append(idS)
+                    studentName.append(nameS)
+                    studentMarks.append(marksS)
+            elif stat == 2:     # Delete
+                if idS and nmS and mkS and int(ids) != -1:
+                    cur.execute(f"DELETE FROM studentTable WHERE studentId = {int(idS)};")
+                    cur.execute("SELECT * FROM studentTable")
+                for (idS, nameS, marksS) in cur:
+                    studentId.append(idS)
+                    studentName.append(nameS)
+                    studentMarks.append(marksS)
+            elif stat == 3:     # Select
+                cur.execute("SELECT * FROM studentTable")
+                for (idS, nameS, marksS) in cur:
+                    studentId.append(idS)
+                    studentName.append(nameS)
+                    studentMarks.append(marksS)
+                if idS and int(ids) != -1:
+                    i = 0
+                    while i < studentId.count() and studentID[i] != int(ids)
+                        i += 1
+                    if studentID[i] == idS
+                        num = i
+    else:
+        cur.execute("SELECT * FROM studentTable")
+        for (idS, nameS, marksS) in cur:
+            studentId.append(idS)
+            studentName.append(nameS)
+            studentMarks.append(marksS)
+
+    
+    
+    return render_template("edit.html", idS=studentId, nameS=studentName, markS=studentMarks, index = num)
 
 if __name__ == '__main__':
     app.run(debug=True)
